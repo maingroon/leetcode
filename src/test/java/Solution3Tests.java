4d92a36aa90a5b269f242a3e6a5a4067a61205fd
Oleksandr Borovyk <maingroon@gmail.com>
2025-10-24 22:56:25 +0300

    Solution 3

---
+++ src/test/java/Solution3Tests.java
@@ -0,0 +1,27 @@
+import com.maingroon.Solution3;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Solution3Tests {
+
+    private final Solution3 solution = new Solution3();
+
+    @Test
+    void test1() {
+        String input = "abcabcbb";
+        assertEquals(3, solution.lengthOfLongestSubstring(input));
+    }
+
+    @Test
+    void test2() {
+        String input = "bbbbb";
+        assertEquals(1, solution.lengthOfLongestSubstring(input));
+    }
+
+    @Test
+    void test3() {
+        String input = "pwwkew";
+        assertEquals(3, solution.lengthOfLongestSubstring(input));
+    }
+}
