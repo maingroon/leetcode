d855a96f69a04cd4d77c030507152acfd5bbf401
Oleksandr Borovyk <maingroon@gmail.com>
2025-10-30 23:26:13 +0200

    Solution 1526

---
+++ src/main/java/com/maingroon/Solution1526.java
@@ -0,0 +1,12 @@
+package com.maingroon;
+
+public class Solution1526 {
+
+    public int minNumberOperations(int[] target) {
+        int operations = target[0];
+        for (int idx = 1; idx < target.length; idx++) {
+            operations += Math.max(0, target[idx] - target[idx - 1]);
+        }
+        return operations;
+    }
+}
---
+++ src/test/java/Solution1526Tests.java
@@ -0,0 +1,29 @@
+import com.maingroon.Solution1526;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Solution1526Tests {
+
+    private final Solution1526 solution = new Solution1526();
+
+    @Test
+    void test1() {
+        int[] input = new int[]{1, 2, 3, 2, 1};
+        assertEquals(3, solution.minNumberOperations(input));
+    }
+
+
+    @Test
+    void test2() {
+        int[] input = new int[]{3, 1, 1, 2};
+        assertEquals(4, solution.minNumberOperations(input));
+    }
+
+
+    @Test
+    void test3() {
+        int[] input = new int[]{3, 1, 5, 4, 2};
+        assertEquals(7, solution.minNumberOperations(input));
+    }
+}
