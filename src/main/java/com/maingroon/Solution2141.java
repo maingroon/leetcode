1d09a317ef13ae5cede70324e4e4aa2ca120a5c4
Oleksandr Borovyk <54804932+maingroon@users.noreply.github.com>
2025-12-01 14:36:38 +0200

    Solution 2141

---
+++ src/main/java/com/maingroon/Solution2141.java
@@ -0,0 +1,25 @@
+package com.maingroon;
+
+class Solution2141 {
+    public long maxRunTime(int n, int[] batteries) {
+        Arrays.sort(batteries);
+        long extra = 0;
+        for (int i = 0; i < batteries.length - n; i++) {
+            extra += batteries[i];
+        }
+
+        int[] live = new int[n];
+        for (int i = 0; i < n; i++) {
+            live[i] = batteries[batteries.length - n + i];
+        }
+
+        for (int i = 0; i < n - 1; i++) {
+            int powerUsed = (i + 1) * (live[i + 1] - live[i]);
+            if (extra < powerUsed) {
+                return live[i] + extra / (i + 1);
+            }
+            extra -= powerUsed;
+        }
+        return live[n - 1] + (extra / n);
+    }
+}
