b1b3e98d0668d68aa614164ecb379bd3c42545d8
Oleksandr Borovyk <maingroon@gmail.com>
2025-12-19 00:11:05 +0200

    Solution 3652

--- src/main/java/com/maingroon/Solution3652.java
+++ src/main/java/com/maingroon/Solution3652.java
@@ -3,41 +3,32 @@ package com.maingroon;
 public class Solution3652 {
 
     public long maxProfit(int[] prices, int[] strategy, int k) {
-        int n = prices.length;
-        long profit = 0;
-        for (int i = 0; i < n; i++) {
-            profit += (long) prices[i] * strategy[i];
+        int length = prices.length;
+        long base = 0;
+        for (int i = 0; i < length; i++) {
+            base += (long) prices[i] * strategy[i];
         }
 
-        long modProfit = profit;
-        for (int i = 0; i < k / 2; i++) {
-            modProfit -= (long) prices[i] * strategy[i];
-        }
-        for (int i = k / 2; i < k; i++) {
-            if (strategy[i] == 0) {
-                modProfit += prices[i];
-            } else if (strategy[i] == -1) {
-                modProfit += 2L * prices[i];
+        int half = k / 2;
+        long prev = 0;
+        long next = 0;
+        for (int i = 0; i < k; i++) {
+            prev += (long) prices[i] * strategy[i];
+            if (i >= half) {
+                next += prices[i];
             }
         }
-        profit = Math.max(profit, modProfit);
 
-        for (int i = k; i < n; i++) {
-            modProfit += (long) prices[i - k] * strategy[i - k];
-            if (strategy[i - (k / 2)] == 0) {
-                modProfit -= prices[i - (k / 2)];
-            } else if (strategy[i - (k / 2)] == -1) {
-                modProfit -= 2L * prices[i - (k / 2)];
-            }
+        long max = Math.max(0, next - prev);
+        for (int i = k; i < length; i++) {
+            prev += (long) prices[i] * strategy[i];
+            prev -= (long) prices[i - k] * strategy[i - k];
 
-            modProfit -= (long) prices[i - k + 1] * strategy[i - k + 1];
-            if (strategy[i - (k / 2) + 1] == 0) {
-                modProfit += prices[i - (k / 2) + 1];
-            } else if (strategy[i - (k / 2) + 1] == -1) {
-                modProfit += 2L * prices[i - (k / 2) + 1];
-            }
-            profit = Math.max(profit, modProfit);
+            next += prices[i];
+            next -= prices[i - k + half];
+
+            max = Math.max(max, next - prev);
         }
-        return profit;
+        return base + max;
     }
 }
